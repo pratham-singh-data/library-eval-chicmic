8184cb43473fc8740cefa92a3ccdaa44cbef060f
Pratham Singh <pratham.singh@chcicmic.co.in>
2023-04-03 19:19:23 +0530

    Grammar fixed

--- app/utils/messages.js
+++ app/utils/messages.js
@@ -1,8 +1,8 @@
 module.exports = {
     VALID_TOKEN_NEEDED: `A valid token is needed.`,
-    INVALID_TOKENTYPE: `This type of token is invalid`,
-    CREDENTIALS_COULD_NOT_BE_VERIFIED: `Credentials could not be verified`,
-    EMAIL_ALREADY_IN_USE: `This email is already in use`,
+    INVALID_TOKENTYPE: `This type of token is invalid.`,
+    CREDENTIALS_COULD_NOT_BE_VERIFIED: `Credentials could not be verified.`,
+    EMAIL_ALREADY_IN_USE: `This email is already in use.`,
     DATA_SUCCESSFULLY_CREATED: `Data successfully created.`,
     DATA_SUCCESSFULLY_UPDATED: `Data successfully updated.`,
     DATA_SUCCESSFULLY_DELETED: `Data successfully deleted.`,
